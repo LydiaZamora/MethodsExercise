0427a8606b800408a150bc83832efaa3e3cdb710
Lydia Zamora <lydiazamorayoga@gmail.com>
2024-06-06 13:36:48 -0700

    called MadLib method

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //MadLib();
+            MadLib();
 
             int added = Add(2, 2);
             Console.WriteLine(added);
